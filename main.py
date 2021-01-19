3848a96b3dd44b19c652e6bce385c4804b06a06f
Ju Dantas <jdantas@thoughtworks.com>
2021-01-19 20:44:51 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def imprimir_tributos (icms, ipi, pis, cofins) :
+  print("--- tributos ---")
+  print(f"ICMS: {icms}")
+  print(f"IPI: {ipi}")
+  print(f"PIS: {pis}")
+  print(f"COFINS: {cofins}")
+
+
+
+
+
+
+
+
+
+
