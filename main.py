9e9e944ea864236ad37c6b9244f7cc17fa4da0a5
Ju Dantas <jdantas@thoughtworks.com>
2021-01-19 21:01:19 -0300

    adicionada novas funcoes para calcular imposto

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 def imprimir_tributos (icms, ipi, pis, cofins) :
   print("--- tributos ---")
   print(f"ICMS: {icms}")
@@ -6,9 +7,30 @@ def imprimir_tributos (icms, ipi, pis, cofins) :
   print(f"COFINS: {cofins}")
 
 
+def icms (qualquer):
+  return qualquer * 0.18
 
 
+def ipi (preco):
+  return preco * 0.04
 
+
+def pis (preco):
+  return preco * 0.0186
+
+
+def cofins (preco):
+  return preco * 0.0854
+
+quantidade = 400
+preco = 4.50
+
+valor_icms = icms(preco * quantidade)
+valor_ipi = ipi(preco * quantidade)
+valor_pis = pis(preco * quantidade)
+valor_cofins = cofins(preco * quantidade)
+
+imprimir_tributos(valor_icms, valor_ipi, valor_pis,valor_cofins)
 
 
 
